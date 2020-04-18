e384be20d146c3981dc8f390e11a773bf1fbd415
Matt Roberts <code@rbrts.uk>
2020-04-18 10:11:51 +0100

    debug: Log values on callback

--- routes/auth.js
+++ routes/auth.js
@@ -18,6 +18,7 @@ router.get('/login', passport.authenticate('auth0', {
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
+    console.log(err, user, info);
     if (err) { return next(err); }
     if (!user) { return res.redirect('/login'); }
     req.logIn(user, function (err) {
